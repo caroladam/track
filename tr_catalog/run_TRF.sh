6ae292f754e79cf631602a5b958be06982a7cae1
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-11-04 12:36:15 -0800

    Update run_TRF.sh

--- tr_catalog/run_TRF.sh
+++ tr_catalog/run_TRF.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 -g <path_to_fasta_files> -t <path_to_trf_executable>"
+    echo "Usage: $0 -t <path_to_trf_executable> -g <fasta_dir>"
     exit 1
 }
 
 # Parse options
-while getopts "g:t:" opt; do
+while getopts "t:g:" opt; do
     case ${opt} in
-        g) genomes_dir="$OPTARG" ;;
         t) trf_dir="$OPTARG" ;;
+	g) fasta_dir="$OPTARG" ;;
         *) usage ;;
     esac
 done
 
 # Check if all arguments are provided
-if [ -z "$genomes_dir" ] || [ -z "$trf_dir" ]; then
+if [ -z "$trf_dir" ] || [ -z "$fasta_dir" ]; then
     echo "Insufficient arguments."
     usage
 fi
 
-echo "Running TRF"
+trim_path() {
+
+        echo "$1" | sed 's/\/$//'
+}
+
+fasta_dir=$( trim_path "$fasta_dir" )
+
+# Split each .fa or .fasta file into chromosomes
+for reference in "$fasta_dir"/*.{fa,fasta}
+do
+    if [ -f "$reference" ]; then
+        faidx -x "$reference"
+    fi
+done
+
+mkdir -p split_chr_fa
+mv *.fa split_chr_fa
 
 # TRF configuration
 matchscore=2
@@ -32,13 +48,26 @@ minscore=24
 maxperiod=2000
 
 # Run TRF on each .fa file in the genomes directory
-for fasta_file in "$genomes_dir"/*.fa; do
-    if [ ! -e "$fasta_file" ]; then
-        echo "No .fa files found in the directory."
-        exit 1
-    fi
-    
-    "$trf_dir"/trf "$fasta_file" "$matchscore" "$mismatchscore" "$indelscore" "$pm" "$pi" "$minscore" "$maxperiod" -f -d -h -l 6
+for fasta_file in split_chr_fa/*.fa
+do
+	if [ ! -f "$fasta_file" ]; then
+		echo "No .fa files found in the directory."
+		exit 1
+	fi
+		echo ""
+		echo "Running TRF on $fasta_file with the following parameters:"
+		echo "matchscore=$matchscore mismatchscore=$mismatchscore indelscore=$indelscore matchprobability=$pm indelprobability=$pi minscore=$minscore maxperiod=$maxperiod"
+
+		"$trf_dir"/trf "$fasta_file" "$matchscore" "$mismatchscore" "$indelscore" "$pm" "$pi" "$minscore" "$maxperiod" -f -d -h -l 6
 done
 
-echo "TRF processing completed for all .fa files in $genomes_dir."
+mkdir -p trf_results
+
+for file in *.dat; do
+	if [[ $file =~ ^(chr[a-zA-Z0-9]+\.fa)\.[0-9]+.*\.dat$ ]]; then
+        	new_name="${BASH_REMATCH[1]}.dat"
+	        mv "$file" "$new_name"
+	fi
+done
+
+mv *.dat trf_results/
