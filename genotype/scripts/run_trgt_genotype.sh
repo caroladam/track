9f9d6baa6bf1b3c59ecc8c1226994c54581eaf69
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-11-04 15:55:42 -0800

    Update run_trgt_genotype.sh

--- genotype/scripts/run_trgt_genotype.sh
+++ genotype/scripts/run_trgt_genotype.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 -r <reference_genome_file> -t <tandem_repeat_catalog> -b <bam_file> -p <output_prefix> -n <threads> -g <trgt_path>"
+    echo "Usage: $0 -r <reference_genome_file> -t <tandem_repeat_catalog> -b <bam_file> -p <output_prefix> -n <threads>"
     exit 1
 }
 
 # Parse command-line options
-while getopts "r:t:b:p:n:g:" opt; 
+while getopts "r:t:b:p:n:" opt; 
 do
     case ${opt} in
         r) ref_file="$OPTARG" ;;
@@ -14,17 +14,16 @@ do
         b) bam_file="$OPTARG" ;;
         p) output_prefix="$OPTARG" ;;
         n) threads="$OPTARG" ;;
-        g) trgt_path="$OPTARG" ;;
         *) usage ;;
     esac
 done
 
 # Check if all required arguments are provided
-if [ -z "$ref_file" ] || [ -z "$tr_catalog" ] || [ -z "$bam_file" ] || [ -z "$output_prefix" ] || [ -z "$threads" ] || [ -z "$trgt_path" ]; then
+if [ -z "$ref_file" ] || [ -z "$tr_catalog" ] || [ -z "$bam_file" ] || [ -z "$output_prefix" ] || [ -z "$threads" ]; then
     usage
 fi
 
 echo "Running TRGT genotype function"
-"$trgt_path"/trgt genotype --genome "$ref_file" --repeats "$tr_catalog" --reads "$bam_file" --threads "$threads" --output-prefix "$output_prefix"
+trgt genotype --genome "$ref_file" --repeats "$tr_catalog" --reads "$bam_file" --threads "$threads" --output-prefix "$output_prefix"
 
 echo "Genotyping completed. VCF and BAM outputs saved with prefix $output_prefix"
