c4651b68c3fbbafc605bf3debc62a0160b687905
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-11-05 13:03:16 -0800

    Update run_TRF.sh

--- tr_catalog/scripts/run_TRF.sh
+++ tr_catalog/scripts/run_TRF.sh
@@ -1,41 +1,25 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 -g <fasta_dir>"
+    echo "Usage: $0 -f <fasta_file>"
     exit 1
 }
 
 # Parse options
-while getopts "g:" opt; do
+while getopts "f:" opt; do
     case ${opt} in
-	g) fasta_dir="$OPTARG" ;;
+	f) fasta_file="$OPTARG" ;;
         *) usage ;;
     esac
 done
 
 # Check if all arguments are provided
-if [ -z "$fasta_dir" ]; then
+if [ -z "$fasta_file" ]; then
     echo "Insufficient arguments."
     usage
 fi
 
-trim_path() {
-
-        echo "$1" | sed 's/\/$//'
-}
-
-fasta_dir=$( trim_path "$fasta_dir" )
-
-# Split each .fa or .fasta file into chromosomes
-for reference in "$fasta_dir"/*.{fa,fasta}
-do
-    if [ -f "$reference" ]; then
-        faidx -x "$reference"
-    fi
-done
-
-mkdir -p split_chr_fa
-mv *.fa split_chr_fa
+mkdir -p trf_results
 
 # TRF configuration
 matchscore=2
@@ -46,21 +30,14 @@ pi=10
 minscore=24
 maxperiod=2000
 
-# Run TRF on each .fa file in the genomes directory
-for fasta_file in split_chr_fa/*.fa
-do
-	if [ ! -f "$fasta_file" ]; then
-		echo "No .fa files found in the directory."
-		exit 1
-	fi
-		echo ""
-		echo "Running TRF on $fasta_file with the following parameters:"
-		echo "matchscore=$matchscore mismatchscore=$mismatchscore indelscore=$indelscore matchprobability=$pm indelprobability=$pi minscore=$minscore maxperiod=$maxperiod"
+echo ""
+echo "Running TRF on $fasta_file with the following parameters:"
+echo "matchscore=$matchscore mismatchscore=$mismatchscore indelscore=$indelscore matchprobability=$pm indelprobability=$pi minscore=$minscore maxperiod=$maxperiod"
 
-		trf "$fasta_file" "$matchscore" "$mismatchscore" "$indelscore" "$pm" "$pi" "$minscore" "$maxperiod" -f -d -h -l 6
-done
+trf "$fasta_file" "$matchscore" "$mismatchscore" "$indelscore" "$pm" "$pi" "$minscore" "$maxperiod" -f -d -h -l 6
+mv *.dat trf_results/ 2>/dev/null
 
-mkdir -p trf_results
+cd trf_results || exit
 
 for file in *.dat; do
 	if [[ $file =~ ^(chr[a-zA-Z0-9]+\.fa)\.[0-9]+.*\.dat$ ]]; then
@@ -69,4 +46,5 @@ for file in *.dat; do
 	fi
 done
 
-mv *.dat trf_results/
+echo ""
+echo "TRF processing is complete! Find your TR data per chromosome at trf_results directory"
