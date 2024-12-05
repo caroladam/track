c177a5f3f47fa40653a12e28580f16a250e86d4f
Carolina Adam <63562718+caroladam@users.noreply.github.com>
2024-12-05 12:58:49 -0800

    Update setup.sh
    Updated download link for chain files

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Example chain files for homology assessment step
-FILE1_URL="https://www.dropbox.com/scl/fi/tqnvtxb4u0dmwg5z36vbi/homo_to_pantro.chain?rlkey=g3tcrkgwe9q9qnn276i4bdwes&st=9findjgi&dl=0"
-FILE2_URL="https://www.dropbox.com/scl/fi/r8v3tu9aab5zvkx3o5yuc/pantro_to_homo.chain?rlkey=ppyoe3ntmueqc64zf07jfwji7&st=zit7dpei&dl=0"
+FILE1_URL="https://www.dropbox.com/scl/fi/4uboym65oz5uckfjttdpl/pantro_to_homo.chain?rlkey=odur0au6xr0l8guxts0sx7v65&st=2t6z8anv&dl=0"
+FILE2_URL="https://www.dropbox.com/scl/fi/6v1do097cwe8flwzf3jit/homo_to_pantro.chain?rlkey=a5x9hbqlpem21lbubwds2z5oz&st=meezav25&dl=0"
 
 # Target directory for chain file download
 TARGET_DIR="homology/data"  
